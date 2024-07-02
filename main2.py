30d9e57d9e18eb718ae53b672b83f31ef0625e49
jshu <jshu@jshus-air.lan>
2024-07-01 21:40:12 -0400

    clean

--- main2.py
+++ main2.py
@@ -1,9 +1,9 @@
-from os import path
 from argparse import ArgumentParser
 from pathlib import Path
 from shutil import copy
 from send2trash import send2trash
 from subprocess import call
+from time import sleep
 
 
 if __name__ == "__main__":
@@ -29,30 +29,38 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # validate
-    destination = Path(args.destination)
+    destination = Path(args.destination)  # /Volumes/OpenSwim
     if not destination.exists():
         raise Exception(f"{arg} does not exist")
     if not destination.is_dir():
         raise Exception(f"{arg} is not a directory")
 
     # create dir with name
-    source = Path(args.name)
-    (source).mkdir(parents=True, exist_ok=True)
+    temp = Path(args.name)  # .../Logic - College Park
+    (temp).mkdir(parents=True, exist_ok=True)
 
     # run spotdl in dir
-    status = call(f"spotdl download {args.link}", cwd=source, shell=True)
+    try:
+        status = call(f"spotdl download {args.link}", cwd=temp, shell=True)
+    except Exception as e:
+        print(f"error: {e}")
 
     # create destination folder with the source basename
-    (destination / source.name).mkdir(parents=True, exist_ok=True)
+    (destination / temp.name).mkdir(
+        parents=True, exist_ok=True
+    )  # /Volumes/OpenSwim/Logic - College Park
 
     # copy files to destination in numerical order
-    files = [f.name for f in source.iterdir() if not f.name.startswith(".")]
-    files.sort(key=lambda f: f.split(" ")[0])  # 1 file.mp3, 2 file.mp3, etc.
+    files = [f.name for f in temp.iterdir() if not f.name.startswith(".")]
+    files.sort(key=lambda f: f.split(" ")[0])  # 1 file.mp3, 2 file.mp3
     for name in files:
-        source_file = source / name
-        dest_file = destination / source.name / name
+        source_file = temp / name  # .../Logic - College Park/1 file.mp3
+        dest_file = (
+            destination / temp.name / name
+        )  # /Volumes/OpenSwim/Logic - College Park/1 file.mp3
         print(f"copying: {name}")
         copy(source_file, dest_file)
+        sleep(0.1)
 
     # remove source
-    send2trash(str(source))
+    send2trash(str(temp))
